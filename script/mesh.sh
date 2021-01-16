1f009233448520de3387bc116bc1e3af8b6ce884
Markku Salkola <salkola@fb.com>
2021-01-15 19:33:13 -0800

    Future proof download and use of mesh descriptor

--- script/mesh.sh
+++ script/mesh.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #
-# Download the MeSH descriptors to data/mesh.
+# Download the MeSH descriptors to data/mesh. The first argument is
+# the optional latest production year with the default value "2021".
 #
-# ./script/mesh.sh
+# ./script/mesh.sh [<year>]
 
 set -eu
 
-DESCRIPTOR=desc2021.xml
+PRODUCTION_YEAR=${1:-"2021"}
+DESCRIPTOR=desc${PRODUCTION_YEAR}.xml
 
-curl ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/${DESCRIPTOR} -o data/mesh/${DESCRIPTOR}
+if ! curl ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/xmlmesh/"$DESCRIPTOR" -o data/mesh/descriptor.xml
+then
+  echo "MeSH descriptor download failed; the latest production year may be old: $PRODUCTION_YEAR"
+  exit 1
+fi
